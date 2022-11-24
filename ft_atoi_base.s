64758aa353a0986b65bd9c99dc34e2618e1f7127
Taha Ed-dafili <ted-dafi@e2r9p4.1337.ma>
2022-11-24 16:03:53 +0100

    strdup comments

--- ft_atoi_base.s
+++
@@ -1,8 +0,0 @@
-globale _ft_atoi_base
-extern	_ft_strlen
-
-
-section .text
-	_check_base:
-		
-	_ft_atoi_base:
--- ft_strdup.s
+++ ft_strdup.s
@@ -6,20 +6,20 @@ extern	___error
 
 section .text
 	_ft_strdup:
-		push	rdi
-		call	_ft_strlen
-		inc		rax
-		mov		rdi, rax
-		call	_malloc
-		jc		err_hand
-		pop		rsi
-		mov		rdi, rax
-		call	_ft_strcpy
-		ret
+		push	rdi			; pushing the address of the s1 to the stack
+		call	_ft_strlen	; calling strlen
+		inc		rax			; incement the len by one
+		mov		rdi, rax	; loading the size of allocation into rdi for malloc
+		call	_malloc		; calling malloc
+		jc		err_hand	; jmp to err_hand, if malloc failed and sets the carry bit
+		pop		rsi			; loading the address of s1 to rsi
+		mov		rdi, rax	; loading the address allocated by malloc to rdi
+		call	_ft_strcpy	; calling strcpy
+		ret					; return
 	err_hand:
-		push	rax
-		call	___error
-		pop		rdi
-		mov		[rax], rdi
-		mov		rax, 0
+		push	rax			; saving the value of rax for later use
+		call	___error	; calling __error
+		pop		rdi			; loading rdi with the value in the stack (from rax before the call)
+		mov		[rax], rdi	; asign the value of rdi to errno
+		mov		rax, 0		; returning NULL
 		ret
